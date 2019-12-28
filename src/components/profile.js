09f310a2e5d494292fdc6508f970360eb5058b89
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2019-12-28 08:29:11 -0300

    Foi criado uma lista na pagina Profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react'
-import { Text, View, Alert, StyleSheet, Image } from 'react-native'
+import { Text, View, Alert, StyleSheet, Image, Picker } from 'react-native'
 
 export default class Profile extends Component {
+
+    state = {
+        programador: ''
+    }
 
     render() {
 
         return (
             <View style={styles.container}>
-                <Image 
-                    source={require('../assets/logoFillipe/fillipeAnjos.jpg')} 
-                    style={styles.logoFillipe}
-                />
-
-                <Text style={styles.textoHome}>Profile</Text>
+                <Text style={styles.profile}>Profile</Text>
                 
+                <Picker
+                    style={styles.pickerComponente}
+                    selectedValue={this.state.programador}
+                    onValueChange={
+                        (itemValor, itemIndex) =>
+                            this.setState({
+                                programador: itemValor
+                            })
+                    }
+                >
+
+                    <Picker.Item label="Escolha a profissão: " value="" />
+                    <Picker.Item label="Desenvolvedor PHP" value="Desenvolvedor PHP" />
+                    <Picker.Item label="Desenvolvedor JAVA" value="Desenvolvedor JAVA" />
+                    <Picker.Item label="Desenvolvedor React Native" value="Desenvolvedor React Native" />
+                    <Picker.Item label="Desenvolvedor Ionic" value="Desenvolvedor Ionic" />
+                    <Picker.Item label="Desenvolvedor NodeJs" value="Desenvolvedor NodeJs" />
+                    <Picker.Item label="Desenvolvedor Ruby" value="Desenvolvedor Ruby" />
+
+                </Picker>
+
+                <Text style={styles.textSelecionado}>
+                    {this.state.programador}
+                </Text>
+
             </View>
         )
     }
@@ -22,17 +46,28 @@ export default class Profile extends Component {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        backgroundColor: "#000000",
+        backgroundColor: "#fff",
         justifyContent: 'center',
         alignItems: 'center',
     },
-    textoHome: {
-        color: '#fff',
+    profile: {
+        color: '#000000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
     },
     logoFillipe: {
         width: 150,
         height: 150,
         borderRadius: 100,
+    },
+    pickerComponente: {
+        width: 350,
+    },
+    textSelecionado: {
+        fontSize: 20,
+        padding: 10,
+        fontWeight: 'bold',
     },
 })
 
