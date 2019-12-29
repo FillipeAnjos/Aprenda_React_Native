d4ad7ac03a6120e87d0c0d7f48337fef1b4926da
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2019-12-29 07:50:09 -0300

    Atualizando o loading da pagina Lista

--- src/components/lista.js
+++ src/components/lista.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
-import { Text, View, Alert, StyleSheet, Image, FlatList } from 'react-native'
+import { Text, View, Alert, StyleSheet, Image, FlatList, ActivityIndicator } from 'react-native'
 
 export default class Lista extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            loading: true,
             data: []
         }
     }
 
     loadUsers = () => {
-        fetch("https://randomuser.me/api/?results=100")
+        fetch("https://randomuser.me/api/?results=1000")
             .then( res => res.json() )
             .then( res => {
                 this.setState({
-                    data: res.results || []
+                    data: res.results || [],
+                    loading: false
                 })
             })
     }
@@ -26,37 +28,58 @@ export default class Lista extends Component {
     }
 
     render() {
+
+        if(this.state.loading){
+
+            return (
+                <View style={styles.containerLoading}>
+                    <ActivityIndicator 
+                        size= 'large'
+                        color = '#2E64FE'
+                    />
+                    <Text>Aguarde, carregando os dados da API ...</Text>
+                </View>
+            )
+
+        }else{
     
-        return (
-            <View style={styles.container}>
-                <Text style={styles.lista}>Lista</Text>
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.lista}>Lista</Text>
 
-                <FlatList
-                    data={this.state.data}
-                    renderItem={({item}) => (
+                    <FlatList
+                        data={this.state.data}
+                        renderItem={({item}) => (
 
-                        <View style={styles.line}>
-                            <Image 
-                                source={{ uri: item.picture.thumbnail }}
-                                style={styles.avatar}
-                            />
+                            <View style={styles.line}>
+                                <Image 
+                                    source={{ uri: item.picture.thumbnail }}
+                                    style={styles.avatar}
+                                />
 
-                            <View style={styles.info}>
-                                <Text style={styles.email}>{item.email}</Text>
-                                <Text style={styles.name}>{item.name.first} {item.name.last}</Text>
+                                <View style={styles.info}>
+                                    <Text style={styles.email}>{item.email}</Text>
+                                    <Text style={styles.name}>{item.name.first} {item.name.last}</Text>
+                                </View>
                             </View>
-                        </View>
 
-                    )}
-                    keyExtractor={ item => item.email }
-                />
+                        )}
+                        keyExtractor={ item => item.email }
+                    />
 
-            </View>
-        )
+                </View>
+            )
+
+        }
     }
 }
 
 const styles = StyleSheet.create({
+    containerLoading: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     container:{
         flex: 1,
         backgroundColor: "#fff",
